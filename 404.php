2c839bada1c5f6bf47f58653d012b610e96f4d24
ElskaJo <elska.jo@gmail.com>
2021-03-08 11:48:00 -0800

    - fixing finally the flash on load seen on buttons sitewide - culprit was animation fading out the original bg color - adding basic form styles to comments - adding border to footer to create more distinction - increasing min width for p tags - adjusting button and parent padding - testing adding variables in theme file where multiple colors exist - styling 404 page - adjusting article title link color - reminder to self this was done during a cat5 migraine. - so cut yourself some slack later.

--- 404.php
+++ 404.php
@@ -1,14 +1,19 @@
 <?php get_header(); ?>
-<main id="content">
-<article id="post-0" class="post not-found">
-<header class="header">
-<h1 class="entry-title"><?php esc_html_e( 'Not Found', 'snowfall' ); ?></h1>
-</header>
-<div class="entry-content">
-<p><?php esc_html_e( 'Nothing found for the requested page. Try a search instead?', 'snowfall' ); ?></p>
-<?php get_search_form(); ?>
-</div>
-</article>
+
+<main class="generic__missing-page" role="main">
+    <article id="post-0" class="primary-grid small-grid">
+        <header class="form__header">
+            <h1 class="entry-title"><?php esc_html_e( 'Not Found', 'snowfall' ); ?></h1>
+        </header>
+        <div class="form__message">
+            <p>I'm sorry, I haven't created that page.</p>
+            <p>There are lots of other pages though, pages that need your attention and pictures of my cat that absolutely require you see them. </p>
+            <p>Head over to 'Play', to see Petunia in all her glory, or use the search below.</p>
+        </div>
+        <div class="form__input">
+            <?php get_search_form(); ?>
+        </div>
+    </article>
 </main>
-<?php get_sidebar(); ?>
+
 <?php get_footer(); ?>
