6c9838ad5573b53c60beba0a8f3e73b209862cb1
ElskaJo <elska.jo@gmail.com>
2021-04-03 09:05:14 -0700

    - adding google analytics code for launch - changing layout a bit on what i do page to break up text - adding spacing to the bracketed list to allow some breathing room - removing unused css found

--- header.php
+++ header.php
@@ -1,6 +1,15 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
+	  <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-136237741-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-136237741-1');
+</script>
 
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1"/>
